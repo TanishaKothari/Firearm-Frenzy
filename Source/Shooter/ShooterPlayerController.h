e0faa15bb3d78e652bc27726b2a8b183feb91002
Tanisha Kothari <kotharitanisha50@gmail.comgit config --global user.email kotharitanisha50@gmail.com>
2025-01-21 19:26:17 +0400

    Core gameplay mechanics and AI features implemented

---
+++ Source/Shooter/ShooterPlayerController.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ShooterPlayerController.generated.h"
+
+
+UCLASS()
+class SHOOTER_API AShooterPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> HUDClass;
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> WinScreenClass;
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> LoseScreenClass;
+	
+	UPROPERTY(EditAnywhere)
+	float RestartDelay = 5;
+
+	FTimerHandle RestartTimer;
+
+	UPROPERTY()
+	UUserWidget* HUD;
+
+public:
+	virtual void GameHasEnded(class AActor *EndGameFocus = nullptr, bool bIsWinner = false) override;
+};
